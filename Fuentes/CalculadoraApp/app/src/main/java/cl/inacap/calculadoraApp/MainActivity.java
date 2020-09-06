e2329bc9d114ca1603c74e238d2a12b512c759e7
Luis Jeria G <luisjeriagarrido@gmail.com>
2020-09-06 20:07:30 -0300

    Generar Hola Mundo
    Genere estructura de proyecto android
    Agregar Paleta de colores del proyecto
    Construir layouts base

---
+++ Fuentes/CalculadoraApp/app/src/main/java/cl/inacap/calculadoraApp/MainActivity.java
@@ -0,0 +1,20 @@
+package cl.inacap.calculadoraApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void saludar(View v){
+        Toast.makeText(this, "Hola al Lucho", Toast.LENGTH_LONG).show();
+    }
+}
